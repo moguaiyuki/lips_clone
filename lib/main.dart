66e04dca71057c9cf443f39d1a48e352b7dc0d6b
moguai <moguai.yuki0424@gmail.com>
2020-05-01 00:24:43 +0900

    Implemented App bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lips Clone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFFED7987),
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -4,7 +4,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ClipOval(
+            child: Image(
+              image: AssetImage('assets/icons/icon_white.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        title: FlatButton(
+          padding: EdgeInsets.symmetric(horizontal: 70),
+          shape: StadiumBorder(),
+          color: Colors.grey[200],
+          onPressed: () {},
+          child: Text(
+            'キーワードで検索',
+            style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 12,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.grey[400],
+                size: 40,
+              ),
+            ),
+          )
+        ],
+      ),
       body: Placeholder(),
     );
   }
