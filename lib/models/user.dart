df6c5d51e06bbaed79bb987e584026a111e8c4e7
moguai <moguai.yuki0424@gmail.com>
2020-05-01 20:15:46 +0900

    Implemented User model

---
+++ lib/models/user.dart
@@ -0,0 +1,8 @@
+class User {
+  final String name;
+  final String profileImageUrl;
+  final String description;
+  final int star;
+
+  User({this.name, this.profileImageUrl, this.description, this.star});
+}
