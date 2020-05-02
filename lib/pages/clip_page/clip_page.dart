be3df16302742bd23e7541a5f71b41969720b4d4
moguai <moguai.yuki0424@gmail.com>
2020-05-02 18:03:06 +0900

    dummy clip page

---
+++ lib/pages/clip_page/clip_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClipPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.yellow,
+      ),
+    );
+  }
+}
