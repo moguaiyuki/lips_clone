7d04949b1a7f60e4850369f46ca1d5983d797d6f
moguai <moguai.yuki0424@gmail.com>
2020-05-03 16:45:38 +0900

    define clip page tabs

---
+++ lib/pages/clip_page/tabs/clip_posts_tab_view.dart/clip_posts_tab_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ClipPostsTabView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image(
+        image: AssetImage('assets/images/no_posts_clip.png'),
+      ),
+    );
+  }
+}
