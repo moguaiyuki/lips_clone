5faff8b0bfbaf92f56b16eef0d3a0823f663ecf6
moguai <moguai.yuki0424@gmail.com>
2020-05-03 21:21:08 +0900

    use default tab controller

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -5,95 +5,74 @@ import '../../utils/styles/styles.dart';
 import 'components/tabs.dart';
 import 'tabs/tabs.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      length: lipsHomeTabItems.length,
-      vsync: this,
-      initialIndex: 3,
-    );
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: LipsColors.brandSub,
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: ClipOval(
-            child: Image(
-              image: AssetImage('assets/icons/icon_white.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        title: FlatButton(
-          padding: EdgeInsets.symmetric(horizontal: 70),
-          shape: StadiumBorder(),
-          color: Colors.grey[200],
-          onPressed: () {},
-          child: Text(
-            'キーワードで検索',
-            style: TextStyle(
-              color: LipsColors.mainTextColor,
-              fontSize: LipsFontSize.xxsmall,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.account_circle,
-                color: Colors.grey[400],
-                size: 40,
+    return DefaultTabController(
+      length: lipsHomeTabItems.length,
+      initialIndex: 3,
+      child: Scaffold(
+        backgroundColor: LipsColors.brandSub,
+        appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ClipOval(
+              child: Image(
+                image: AssetImage('assets/icons/icon_white.png'),
+                fit: BoxFit.cover,
               ),
             ),
-          )
-        ],
-        bottom: TabBar(
-          isScrollable: true,
-          controller: _tabController,
-          labelColor: LipsColors.tabLabelColor,
-          unselectedLabelColor: LipsColors.tabUnselectedLableColor,
-          labelStyle: LipsTextStyle.tablabelStyle,
-          indicator: LipsTabIndicator(),
-          tabs: lipsHomeTabItems,
+          ),
+          title: FlatButton(
+            padding: EdgeInsets.symmetric(horizontal: 70),
+            shape: StadiumBorder(),
+            color: Colors.grey[200],
+            onPressed: () {},
+            child: Text(
+              'キーワードで検索',
+              style: TextStyle(
+                color: LipsColors.mainTextColor,
+                fontSize: LipsFontSize.xxsmall,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Colors.grey[400],
+                  size: 40,
+                ),
+              ),
+            )
+          ],
+          bottom: TabBar(
+            isScrollable: true,
+            labelColor: LipsColors.tabLabelColor,
+            unselectedLabelColor: LipsColors.tabUnselectedLableColor,
+            labelStyle: LipsTextStyle.tablabelStyle,
+            indicator: LipsTabIndicator(),
+            tabs: lipsHomeTabItems,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            RecomendedTabView(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+          ],
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          Placeholder(),
-          Placeholder(),
-          Placeholder(),
-          RecomendedTabView(),
-          Placeholder(),
-          Placeholder(),
-          Placeholder(),
-          Placeholder(),
-          Placeholder(),
-        ],
-      ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _tabController.dispose();
   }
 }
