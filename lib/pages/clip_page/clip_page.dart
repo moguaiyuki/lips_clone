49cdcaa8b5f9111c2ef0bc850fe82a21832c0fb8
moguai <moguai.yuki0424@gmail.com>
2020-05-03 16:47:13 +0900

    Finished Clip page

--- lib/pages/clip_page/clip_page.dart
+++ lib/pages/clip_page/clip_page.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:lips_clone/pages/clip_page/components/tabs.dart';
+import 'package:lips_clone/pages/clip_page/tabs/clip_posts_tab_view.dart/clip_posts_tab_view.dart';
+import 'package:lips_clone/utils/styles/styles.dart';
+import 'package:lips_clone/widgets/lips_tab_indicator.dart';
 
-class ClipPage extends StatelessWidget {
+class ClipPage extends StatefulWidget {
+  @override
+  _ClipPageState createState() => _ClipPageState();
+}
+
+class _ClipPageState extends State<ClipPage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      vsync: this,
+      length: lipsClipTabItems.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 150,
-          height: 150,
-          color: Colors.yellow,
+      backgroundColor: LipsColors.brandSub,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'マイクリップ',
+          style: LipsTextStyle.appBarTitleTextStyle,
         ),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: LipsColors.tabLabelColor,
+          unselectedLabelColor: LipsColors.tabUnselectedLableColor,
+          labelStyle: LipsTextStyle.tablabelStyle,
+          indicator: LipsTabIndicator(xOffset: 10),
+          tabs: lipsClipTabItems,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ClipPostsTabView(),
+          ClipPostsTabView(),
+        ],
       ),
     );
   }
--- lib/utils/styles/styles.dart
+++ lib/utils/styles/styles.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class LipsColors {
   const LipsColors._();
   static const Color brand = Color(0xFFED7987);
-  static const Color brandSub = Color(0xFFF4F3F4);
+  static const Color brandSub = Color(0xFFE7E3E1);
   static const Color mainTextColor = Color(0xFF757575);
 
   static const Color tabLabelColor = Colors.white;
@@ -14,6 +14,11 @@ class LipsTextStyle {
   const LipsTextStyle._();
   static const TextStyle tablabelStyle =
       TextStyle(fontSize: LipsFontSize.xxsmall);
+  static const TextStyle appBarTitleTextStyle = TextStyle(
+    color: Colors.black87,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
 }
 
 class LipsFontSize {
