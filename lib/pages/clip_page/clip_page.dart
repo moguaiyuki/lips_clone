ea540017262724fea5129953a77f763cd0eea94b
moguai <moguai.yuki0424@gmail.com>
2020-05-03 21:23:02 +0900

    use deraul tab controller for clip page

--- lib/pages/clip_page/clip_page.dart
+++ lib/pages/clip_page/clip_page.dart
@@ -1,51 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:lips_clone/pages/clip_page/components/tabs.dart';
-import 'package:lips_clone/pages/clip_page/tabs/clip_posts_tab_view.dart/clip_posts_tab_view.dart';
-import 'package:lips_clone/utils/styles/styles.dart';
-import 'package:lips_clone/widgets/lips_tab_indicator.dart';
 
-class ClipPage extends StatefulWidget {
-  @override
-  _ClipPageState createState() => _ClipPageState();
-}
+import '../../utils/styles/styles.dart';
+import '../../widgets/lips_tab_indicator.dart';
+import 'components/tabs.dart';
+import 'tabs/clip_posts_tab_view.dart/clip_posts_tab_view.dart';
 
-class _ClipPageState extends State<ClipPage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      vsync: this,
-      length: lipsClipTabItems.length,
-    );
-  }
-
+class ClipPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: LipsColors.brandSub,
-      appBar: AppBar(
-        title: Text(
-          'マイクリップ',
-          style: LipsTextStyle.appBarTitleTextStyle,
+    return DefaultTabController(
+      length: lipsClipTabItems.length,
+      child: Scaffold(
+        backgroundColor: LipsColors.brandSub,
+        appBar: AppBar(
+          title: Text(
+            'マイクリップ',
+            style: LipsTextStyle.appBarTitleTextStyle,
+          ),
+          bottom: TabBar(
+            labelColor: LipsColors.tabLabelColor,
+            unselectedLabelColor: LipsColors.tabUnselectedLableColor,
+            labelStyle: LipsTextStyle.tablabelStyle,
+            indicator: LipsTabIndicator(xOffset: 10),
+            tabs: lipsClipTabItems,
+          ),
         ),
-        bottom: TabBar(
-          controller: _tabController,
-          labelColor: LipsColors.tabLabelColor,
-          unselectedLabelColor: LipsColors.tabUnselectedLableColor,
-          labelStyle: LipsTextStyle.tablabelStyle,
-          indicator: LipsTabIndicator(xOffset: 10),
-          tabs: lipsClipTabItems,
+        body: TabBarView(
+          children: [
+            ClipPostsTabView(),
+            ClipPostsTabView(),
+          ],
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          ClipPostsTabView(),
-          ClipPostsTabView(),
-        ],
       ),
     );
   }
