2ff2d9a6aad2f2d940605092ad8b39e71fdf4664
moguai <moguai.yuki0424@gmail.com>
2020-05-04 16:36:28 +0900

    ranking tabs

---
+++ lib/pages/ranking_page/components/tabs.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const List<Tab> lipsRankingTabItems = [
+  Tab(text: 'アイ'),
+  Tab(text: 'フェイス'),
+  Tab(text: 'リップ'),
+  Tab(text: 'ネイル'),
+  Tab(text: 'キット'),
+  Tab(text: 'スキンケア'),
+  Tab(text: 'ボディ'),
+  Tab(text: 'ヘアケア'),
+  Tab(text: 'フレグランス'),
+  Tab(text: '美容グッズ'),
+  Tab(text: 'インナーケア'),
+  Tab(text: 'その他'),
+];
--- lib/pages/ranking_page/ranking_page.dart
+++ lib/pages/ranking_page/ranking_page.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:lips_clone/pages/ranking_page/components/ranking_list_view.dart';
+import 'package:lips_clone/pages/ranking_page/components/tabs.dart';
+import 'package:lips_clone/utils/styles/styles.dart';
+import 'package:lips_clone/widgets/lips_tab_indicator.dart';
 
 class RankingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          color: Colors.red,
+    return DefaultTabController(
+      length: lipsRankingTabItems.length,
+      child: Scaffold(
+        backgroundColor: LipsColors.brandSub,
+        appBar: AppBar(
+          title: GestureDetector(
+            onTap: () {},
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'すべてのランキング',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Icon(FlutterIcons.down_ant),
+                ),
+              ],
+            ),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            labelColor: LipsColors.tabLabelColor,
+            unselectedLabelColor: LipsColors.tabUnselectedLableColor,
+            indicator: LipsTabIndicator(xOffset: 5),
+            labelStyle: LipsTextStyle.tablabelStyle,
+            tabs: lipsRankingTabItems,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            RankingListView(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+            Placeholder(),
+          ],
         ),
       ),
     );
