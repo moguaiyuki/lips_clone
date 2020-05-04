5b386396f35032357b4f1fe8378e770fd6fbad82
moguai <moguai.yuki0424@gmail.com>
2020-05-04 15:36:32 +0900

    add properties to item model

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,7 +1,21 @@
+import 'package:flutter/material.dart';
+
 class Item {
   final String imageUrl;
   final String name;
   final String brand;
+  final double star;
+  final int commentCount;
+  final int price;
+  final bool isTaxIncluded;
 
-  Item({this.imageUrl, this.name, this.brand});
+  Item({
+    @required this.imageUrl,
+    @required this.name,
+    @required this.brand,
+    this.star,
+    this.commentCount,
+    this.price,
+    this.isTaxIncluded,
+  });
 }
