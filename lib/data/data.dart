4ce4e1ffcaa53e752e346e30d8dfc2ddb6c1e6d3
moguai <moguai.yuki0424@gmail.com>
2020-05-04 15:35:55 +0900

    Implmeneted function to generate items

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
 import '../models/item.dart';
 import '../models/post.dart';
 import '../models/user.dart';
@@ -20,7 +22,7 @@ final itemNames = [
   'リップジェル',
   'おなかすいた',
   'マーメイド',
-  'エリクシール　フレーバー',
+  'エリクシールフレーバー',
   'グリッタープリズム',
 ];
 
@@ -52,8 +54,21 @@ final posts = List.generate(
     description: "エリクシールさんのバランシングミルクIIで，ついにアンチエイジング始めます🥰",
     item: Item(
       imageUrl: "assets/images/item${random.nextInt(5)}.png",
-      name: itemNames[random.nextInt(4)],
-      brand: itemBrands[random.nextInt(4)],
+      name: itemNames[random.nextInt(5)],
+      brand: itemBrands[random.nextInt(5)],
     ),
   ),
 );
+
+List<Item> generateItems({@required int length}) => List<Item>.generate(
+      length,
+      (index) => Item(
+        name: itemNames[random.nextInt(5)],
+        brand: itemBrands[random.nextInt(5)],
+        imageUrl: "assets/images/cosme${random.nextInt(12)}.png",
+        star: random.nextDouble() * 2 + 3,
+        commentCount: random.nextInt(300),
+        isTaxIncluded: random.nextBool(),
+        price: random.nextInt(3000) + 1000,
+      ),
+    );
